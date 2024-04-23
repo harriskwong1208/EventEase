160364a6100f4c988b9ee70dbd94262785424c64
harris <harriskwong120898@gmail.com>
2024-04-23 16:18:28 -0400

    Created User model and user controller

---
+++ BackEnd/EventEase.Api/EventEase.Api/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace EventEase.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Birthday { get; set; }
+    }
+}
