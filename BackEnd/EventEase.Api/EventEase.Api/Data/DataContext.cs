60c86ef58236d22c2e762de5cd8ad527dabebb1b
harris <harriskwong120898@gmail.com>
2024-05-04 22:00:56 -0400

    Added connection string

--- BackEnd/EventEase.Api/EventEase.Api/Data/DataContext.cs
+++ BackEnd/EventEase.Api/EventEase.Api/Data/DataContext.cs
@@ -4,9 +4,16 @@ namespace EventEase.Api.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContext<DataContext> options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=HARRIS-LAPTOP\\SQLEXPRESS;Database=EventEaseLocal;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
     }
+
 }
